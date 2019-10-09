c0618782fa8168e9965e31fb58e9497816c3853f
Felipe Machado <feelipe@email.com>
2019-10-08 23:47:48 -0300

    projeto agenda-petshop com graphql

---
+++ infraestrutura/crud/cliente.js
@@ -0,0 +1,41 @@
+const executaQuery = require("../database/queries");
+
+class Cliente {
+  lista() {
+    const sql = "SELECT * FROM Clientes";
+
+    return executaQuery(sql);
+  }
+
+  buscaPorId(id) {
+    const sql = `SELECT * FROM Clientes WHERE id=${id}`;
+
+    return executaQuery(sql).then(clientes => clientes[0]);
+  }
+
+  adiciona(item) {
+    const { nome, cpf } = item;
+    const sql = `INSERT INTO Clientes(nome, CPF) VALUES('${nome}', '${cpf}')`;
+
+    return executaQuery(sql).then(resposta => ({
+      id: resposta.insertId,
+      nome,
+      cpf
+    }));
+  }
+
+  atualiza(novoItem, id) {
+    const { nome, cpf } = novoItem;
+    const sql = `UPDATE Clientes SET nome='${nome}', CPF='${cpf}' WHERE id=${id}`;
+
+    executaQuery(sql);
+  }
+
+  deleta(id) {
+    const sql = `DELETE FROM Clientes WHERE id=${id}`;
+
+    executaQuery(sql);
+  }
+}
+
+module.exports = new Cliente();
---
+++ server.js
@@ -0,0 +1,38 @@
+const { GraphQLServer } = require("graphql-yoga");
+const conexao = require("./infraestrutura/conexao");
+const Tabelas = require("./infraestrutura/database/tabelas");
+
+const Operacoes = require("./infraestrutura/operations");
+
+conexao.connect(erro => {
+  if (erro) {
+    console.log(erro);
+  }
+
+  console.log("conectou no banco");
+
+  Tabelas.init(conexao);
+});
+
+const Clientes = new Operacoes("cliente");
+
+const resolvers = {
+  Query: {
+    status: () => "Servidor Rodando!",
+    clientes: () => Clientes.lista(),
+    cliente: (root, { id }) => Clientes.buscaPorId(id)
+  },
+
+  Mutation: {
+    adicionarCliente: (root, params) => Clientes.adiciona(params)
+  }
+};
+
+const server = new GraphQLServer({
+  resolvers,
+  typeDefs: "./schema.graphql"
+});
+
+server.start(() => {
+  console.log("Servidor Ouvindo!");
+});
